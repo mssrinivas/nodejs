9067a6cb4b4022c1cceae9dc47c2c8a0ccec5df6
EC2 Default User <ec2-user@ip-172-31-18-26.us-west-2.compute.internal>
2018-11-03 03:30:34 +0000

    Modifications to MongoDB Connection

--- app.js
+++ app.js
@@ -53,13 +53,19 @@ var db_user = "cmpe281" ;
 var db_pwd  = "cmpe281" ;
 var db_name = "test" ;
 */
-
+/*
 var db_host = (process.env.mongodb_host || "ds231133.mlab.com" ) ;
 var db_port = (process.env.mongodb_port || "31133" ) ;
 var db_user = (process.env.mongodb_user || "cmpe281" ) ;
 var db_pwd  = (process.env.mongodb_pwd  || "cmpe281" ) ;
 var db_name = (process.env.mongodb_name || "mssrinivas-cmpe281" ) ;
-   
+ */
+
+var db_host = (process.env.mongodb_host || "172.31.18.26" ) ;
+var db_port = (process.env.mongodb_port || "27017" ) ;
+var db_user = (process.env.mongodb_user || "cmpe281" ) ;
+var db_pwd  = (process.env.mongodb_pwd  || "cmpe281" ) ;
+var db_name = (process.env.mongodb_name || "cmpe281" ) ; 
 
 var db = new DB(db_name,
                 new DB_Server( db_host, db_port,
